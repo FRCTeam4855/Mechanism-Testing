0c19868175ebf876753263c1e3ecb45af11e67e2
Hayden <Hayden.chimukwa@gmail.com>
2026-04-24 18:32:53 -0400

    Updated Buttons
    Updated Buttons for continued testing

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.subsystems.FlywheelSubsystem;
@@ -31,12 +32,16 @@ public class RobotContainer {
     new JoystickButton(m_controller, 1).onTrue(m_flywheelSubsystem.setVelocity(RPM.of(500)).repeatedly());
     new JoystickButton(m_controller, 2).onTrue(m_flywheelSubsystem.setVelocity(RPM.of(0)).repeatedly());
 
+    new JoystickButton(m_controller, 6).onTrue(new RunCommand(()-> m_flywheelSubsystem.setVelocity(RPM.of(500))));
+    new JoystickButton(m_controller, 8).onTrue(new RunCommand(()-> m_flywheelSubsystem.setVelocity(RPM.of(0))));
+
     //button for customizable RPM setpoint from SmartDashboard
-    new JoystickButton(m_controller, 5).onTrue(m_flywheelSubsystem.setVelocity(RPM.of(SmartDashboard.getNumber("Flywheel Setpoint", 0))));
+    new JoystickButton(m_controller, 5).onTrue(m_flywheelSubsystem.setVelocity(RPM.of(SmartDashboard.getNumber("Flywheel Setpoint (RPM)", 0))));
+    new JoystickButton(m_controller, 10).onTrue(new RunCommand(()-> m_flywheelSubsystem.setVelocity(RPM.of(SmartDashboard.getNumber("Flywheel Setpoint (RPM)", 0)))));
 
     //buttons for adjusting flywheel speed (upping and lowering but 0.3 or -0.3)
-    new JoystickButton(m_controller,3).onTrue(m_flywheelSubsystem.set(0.3));
-    new JoystickButton(m_controller, 4).onTrue(m_flywheelSubsystem.set(-0.3));
+    new JoystickButton(m_controller,3).onTrue(new RunCommand(()-> m_flywheelSubsystem.set(0.3)));
+    new JoystickButton(m_controller, 4).onTrue(new RunCommand(()-> m_flywheelSubsystem.set(-0.3)));
   }
 
   public Command getAutonomousCommand() {
